1358c52b2f38a4abe4202662f383f5f1e62d3f13
Shiv Matta <shiv.matta@gmail.com>
2022-07-27 22:27:39 -0500

    rough draft mvp

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
 .then(response => response.json())
 .then(data => {
 
+    //Create the other UL 
     let bigUL = document.createElement("ul")
-  
+    //Iterare through the array drinks and create a new li element, button, and counter for each drink type
     data.drinks.forEach(drink => {
         let liElem = document.createElement("li");
         let liElemButton = document.createElement("button");
         let liElemCounter = document.createElement("span");
         
+        // Assign an ID to the counter so that it can be reference earlier
         liElemCounter.setAttribute('id', 'counter-'+ drink['idDrink'])
         
-        // Set all Inner
-        liElemCounter.innerHTML = "0"
-        liElemButton.innerHTML = "Upvote"
-        
+        // Set all Inner HTML to starting value
+        liElemCounter.innerHTML = " 0"
+        liElemButton.innerHTML = "Like"
         liElem.innerHTML = drink["strDrink"]
         
 
