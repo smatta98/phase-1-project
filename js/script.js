29366799037d38d0100b486ab829eeb8649930ac
Shiv Matta <shiv.matta@gmail.com>
2022-08-07 15:31:14 -0500

    Appended the button and counter elements to the list and then appended the entire list to the master list

--- js/script.js
+++ js/script.js
@@ -11,12 +11,22 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
        let liElemButton = document.createElement("button");
        let liElemCounter = document.createElement("span");
 
-       // Assign an ID to the counter so that it can be reference later
+       // Assign IDs to the LI elements so that it can be referenced later
+       liElem.setAttribute('id', drink['idDrink'])
         liElemCounter.setAttribute('id', 'counter-'+ drink['idDrink'])
 
          // Set all Inner HTML to starting value
         liElemCounter.innerHTML = "    0"
        liElemButton.innerHTML = EMPTY_HEART
        liElem.innerHTML = drink['idDrink']
+
+
+      //Append the Button and Counter to the list
+       liElem.appendChild(liElemButton)
+       liElem.appendChild(liElemCounter)
+
+       //Append the list to the Master list
+       bigUL.appendChild(liElem)
+
    })
 })
