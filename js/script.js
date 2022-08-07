2fb011d088ec782c70e3200827f5f97400be3cbd
Shiv Matta <shiv.matta@gmail.com>
2022-08-07 15:24:36 -0500

    Create a list (UL) and iterate through the array to create li elements

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
 .then(response => response.json())
 .then(data => {
    console.log(data)
+   
+   //Create the other UL
+   let bigUL = document.createElement("ul")
+   //Iterare through the array drinks and create a new li element, button, and counter for each drink type
+   data.drinks.forEach(drink => {
+       let liElem = document.createElement("li");
+       let liElemButton = document.createElement("button");
+       let liElemCounter = document.createElement("span");
+   })
 })
