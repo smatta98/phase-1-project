38a3742754220e36d1b13302b69c7294da02dfad
Shiv Matta <shiv.matta@gmail.com>
2022-08-08 23:56:03 -0500

    Changed title color and edited rReadME

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+let ingArray = [];
 fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
 .then(response => response.json())
 .then(data => {
@@ -8,11 +8,13 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
    
    //Create the other UL
    let bigUL = document.createElement("ul")
+//   
    //Iterare through the array drinks and create a new li element, button, and counter for each drink type
    data.drinks.forEach(drink => {
        let liElem = document.createElement("li");
        let liElemButton = document.createElement("button");
        let liElemCounter = document.createElement("span");
+
 
        // Assign IDs to the li elements so that it can be referenced later
             //    liElem.setAttribute('id', drink['idDrink'])
